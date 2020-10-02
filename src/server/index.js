f52815e45503b058171a8ef9b2841b98c44eed01
Emma Blair <67341919+emmablair@users.noreply.github.com>
2020-10-02 14:04:33 -0400

    cors & body-parser/port8081
    require cors & body-parser. for server side (index.js). port 8081 change.

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,17 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-
+const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(cors())
+// use JSON
+app.use(bodyParser.json())
+// use url encoded values
+app.use(bodyParser.urlencoded({
+    entended: true
+}))
 
 app.use(express.static('dist'))
 
@@ -14,10 +23,16 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
+
+var json = {
+    'title': 'test json response',
+    'message': 'this is a message',
+    'time': 'now'
+}
