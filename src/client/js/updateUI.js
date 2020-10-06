82edc6b231a9dff229718dd3012e32d7e91caae1
Emma Blair <67341919+emmablair@users.noreply.github.com>
2020-10-06 18:02:42 -0400

    update UI with api info

---
+++ src/client/js/updateUI.js
@@ -0,0 +1,9 @@
+const updateUI = (data) => {
+    document.querySelector('#agreement').innerHTML = `${data.agreement.toLowerCase()}`;
+    document.querySelector('#irony').innerHTML = `${data.irony.toLowerCase()}`;
+    document.querySelector('#subjectivity').innerHTML = `${data.subjectivity.toLowerCase()}`;
+    document.querySelector('#confidence').innerHTML = `${data.confidence}%`;
+    document.querySelector('#results').style.display = 'block';
+};
+
+export { updateUI }
