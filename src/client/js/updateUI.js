3b57c23185d42f8d3ae0ad557b32b74ff8bc3b2d
Emma Blair <67341919+emmablair@users.noreply.github.com>
2020-10-07 13:59:51 -0400

    add `confident` to #confidence

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -2,7 +2,7 @@ const updateUI = (data) => {
     document.querySelector('#agreement').innerHTML = `${data.agreement.toLowerCase()}`;
     document.querySelector('#irony').innerHTML = `${data.irony.toLowerCase()}`;
     document.querySelector('#subjectivity').innerHTML = `${data.subjectivity.toLowerCase()}`;
-    document.querySelector('#confidence').innerHTML = `${data.confidence}%`;
+    document.querySelector('#confidence').innerHTML = `${data.confidence}% confident`;
     document.querySelector('#results').style.display = 'block';
 };
 
