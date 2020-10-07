a1d9c7c57a4dc6291c445bcb211f3e4cffa6c6ee
Emma Blair <67341919+emmablair@users.noreply.github.com>
2020-10-07 19:00:54 -0400

    pending/results visibility+cursor

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -3,7 +3,14 @@ const updateUI = (data) => {
     document.querySelector('#irony').innerHTML = `${data.irony.toLowerCase()}`;
     document.querySelector('#subjectivity').innerHTML = `${data.subjectivity.toLowerCase()}`;
     document.querySelector('#confidence').innerHTML = `${data.confidence}% confident`;
-    document.querySelector('#results').style.display = 'block';
+    // change curser back
+    document.querySelector('#submit').style.cursor = 'pointer';
+    document.querySelector('.bg').style.cursor = 'default';
+    // remove pending msg & ADD display results
+    document.querySelector('#label').style.visibility = 'hidden';
+    document.querySelector('#label').style.opacity = '0';
+    document.querySelector('#results').style.visibility = 'visible';
+    document.querySelector('#results').style.opacity = '1';
 };
 
 export { updateUI }
