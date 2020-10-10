be28b1c9f2d308a460e8207dca7b7dbef96bdcbb
Emma Blair <67341919+emmablair@users.noreply.github.com>
2020-10-10 18:33:08 -0400

    Update urlChecker.js

--- src/client/js/urlChecker.js
+++ src/client/js/urlChecker.js
@@ -1,5 +1,5 @@
 const urlChecker = (userInput) => {
-    const validURL = userInput.match('/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g');
+    const validURL = userInput.match(/(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
     // userInput.match(validURL);
     if (validURL == null) {
         console.log('Invalid URL!')
